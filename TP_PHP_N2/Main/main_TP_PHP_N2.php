d6371557b9747a816d79b2c66236534459b97994
Ashkade <betari.laid@gmail.com>
2023-11-02 10:46:42 +0100

    correction du tp php N2

--- TP_PHP_N2/Main/main_TP_PHP_N2.php
+++ TP_PHP_N2/Main/main_TP_PHP_N2.php
@@ -7,11 +7,11 @@ $_mavar, $_5var, $__élément1, $hotel4* ?
 $mavar, $var5, $_mavar, $_5var.
 <br><br>
 Exercice 2<br>
-Donnez les valeurs de $x, $y, $z à la fin du script suivant :
-$x="PostgreSQL";
-$y="MySQL";
-$z=&$x;
-$x="PHP 8";
+Donnez les valeurs de $x, $y, $z à la fin du script suivant :<br>
+$x="PostgreSQL";<br>
+$y="MySQL";<br>
+$z=&$x;<br> <!-- Affectation par référencement -->
+$x="PHP 8";<br>
 $y=&$x;
 <br><br>
 à la fin : 
@@ -22,14 +22,25 @@ $y=&$x;
 Exercice 3<br>
 Lisez les valeurs des variables du script de l’exercice 2 à l’aide du tableau $GLOBALS.
 <br><br>
-foreach($GLOBAL as $name => $value)
+foreach($GLOBALS as $name => $value)
     echo "La variable $name a pour valeur $value ";
+
+    Correction 
+echo $GLOBALS['x'], '<br>';
+    echo $GLOBALS['y'], '<br>';
+    echo $GLOBALS['z'], '<br>';
+    
     <br><br>
 Exercice 4<br><br>
 Déterminez le numéro de version de PHP, le nom du système d’exploitation de votre
 serveur ainsi que la langue du navigateur du poste client.
 <br><br>
 dans cmd php -v
+
+    Correction 
+    <?php echo "Version de PHP : ", PHP_VERSION, "<br>";
+    echo "Version de l'OS : ", PHP_OS, "<br>";
+    echo "Langue du navigateur : ", $_SERVER["HTTP_ACCEPT_LANGUAGE"], "<br>";?>
 <br><br>
 Exercice 5<br>
 Donnez la valeur de chacune des variables pendant et à la fin du script suivant, et
@@ -42,20 +53,28 @@ $x.=$y;<br>
 $y*=$z;<br>
 $a[0]="MySQL";<br>
 <br>
-$x="PHP7";<br>
-var_dump($x);<br>
-$a[]=&$x;<br>
-var_dump($a);<br>
-$y=" 8e version de PHP";<br>
-var_dump($y);<br>
-$z=$y*10;<br>
-var_dump($z);<br>
-$x.=$y;<br>
-var_dump($x);<br>
-$y*=$z;<br>
-var_dump($y);<br>
-$a[0]="MySQL";<br>
-var_dump($a[0]);<br>
+<?php $x="PHP7";
+var_dump($x);
+echo '<br>';
+$a[]=&$x;
+var_dump($a);
+echo '<br>';
+$y=" 8e version de PHP";
+var_dump($y);
+echo '<br>';
+$z=$y*10;
+var_dump($z);
+echo '<br>';
+$x.=$y;
+var_dump($x);
+echo '<br>';
+$y*=$z;
+var_dump($y);
+echo '<br>';
+$a[0]="MySQL";
+var_dump($a[0]);
+echo '<br>';
+?>
 <br>
 
 Exercice 6<br>
@@ -78,11 +97,25 @@ $d=($a OR $b);<br>
 $e=($a AND $c);<br>
 $f=($a XOR $b);<br>
 <br>
-$a = N/A<br>
-$b = N/A<br>
+$a = N/A FALSE<br>
+$b = N/A TRUE<br>
 $c = FALSE<br>
 $d = TRUE<br>
 $e = FALSE<br>
 $f = TRUE<br>
+
+<?php
+
+    define('mavar','carton pate');
+    echo mavar;
+    echo '<br>';
+    $baguette="j'ai acheté 12 baguettes ce matin";
+    $bag = (double) $baguette;
+    $bag1 = (int) filter_var($baguette, FILTER_SANITIZE_NUMBER_INT); 
+    echo $bag;
+    echo '<br>';
+    echo $bag1;
+
+?>
 
 </div>
